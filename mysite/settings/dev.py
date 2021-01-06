7fd0efa1a9e2e4ce0e85f82132f8f9b06c5ee783
Your Name <you@example.com>
2021-01-07 00:41:43 +0100

    new secret key

--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -1,10 +1,11 @@
 from .base import *
+from .secret import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k8n!sid$ehep)9=&anze=06-#f@$kj-@3^5un(i^_lphm7=j9c'
+
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*'] 
