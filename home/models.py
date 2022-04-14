d1d8e5fec54bd4c17e2980b01fce50f09f042d2e
Your Name <you@example.com>
2022-04-14 19:52:31 +0200

    home page done

--- home/models.py
+++ home/models.py
@@ -1,7 +1,56 @@
 from django.db import models
-
 from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import PageChooserPanel, FieldPanel, StreamFieldPanel, MultiFieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.core.fields import StreamField
+from streams import blocks
+
 
 
 class HomePage(Page):
-    pass
+    """ Home page model."""
+
+    template = "home/home_page.html"
+    max_count = 1
+    banner_title = models.CharField(max_length=100, blank=False, null=True)
+    banner_subtitle = models.CharField(max_length=150, blank=False, null=True)
+    banner_text = models.TextField(max_length=1000, blank=False, null=True)
+    banner_button = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    bunner_button_text = models.CharField(max_length=100, blank=False, null=True)
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    services = models.CharField(max_length=100, blank=False, null=True)
+    content = StreamField(
+        [
+            ("cards", blocks.CardBlock())
+        ],
+        null=True,
+        blank=True,
+        # title_and_text и full_richtext названия для админки не переменные
+    )
+
+    content_panels = Page.content_panels +[
+        MultiFieldPanel([
+            FieldPanel("banner_title"),
+            FieldPanel("banner_subtitle"),
+            FieldPanel("banner_text"),
+            PageChooserPanel("banner_button"),
+            FieldPanel("bunner_button_text"),
+            ImageChooserPanel("banner_image"),
+        ], heading="Information"),
+        MultiFieldPanel([
+            FieldPanel("services"),
+            StreamFieldPanel("content"),
+        ], heading="Services")
+    ]
--- streams/blocks.py
+++ streams/blocks.py
@@ -11,3 +11,27 @@ class TitleAndTextBlock(blocks.StructBlock):
         template = 'streams/title_and_text_block.html'
         icon = "edit"
         label = "Title & Text"
+
+class CardBlock(blocks.StructBlock):
+    """ Cards with image text and button """
+
+    
+
+    cards = blocks.ListBlock(
+        blocks.StructBlock(
+            [
+                ("image", ImageChooserBlock(required=True)),
+                ("title", blocks.CharBlock(required=True, max_length=40)),
+                ("text", blocks.TextBlock(required=True, max_length=400)),
+                ("button_page", blocks.PageChooserBlock(required=False)),
+                # here we will use some logic in the templates for choose link
+                ("button_url", blocks.URLBlock(required=False,
+                                               help_text='If the button page above is selected, that will be used first.'))
+
+            ]
+        )
+    )
+    class Meta:  # noqa
+        template = 'streams/card_block.html'
+        icon = "placeholder"
+        label = "Service Cards"
